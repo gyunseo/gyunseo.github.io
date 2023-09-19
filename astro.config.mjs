a122ee546792a443867a8f7b193115e1307a78e5
Gyunseo Lee <Gyunseo Lee>
2023-09-19 17:10:30 +0900

    fix: update astro.config.mjs import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import convertMarkdownImageLink from "./src/utils/convertMarkdownImageLink";
+import convertMarkdownImageLink from "/src/utils/convertMarkdownImageLink";
 // https://astro.build/config
 export default defineConfig({
   site: "https://gyunseo.xyz", // replace this with your deployed dom:wain
