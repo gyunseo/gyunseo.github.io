71071be0f3213282f2321a9f3db70c3cef6e0bd2
Gyunseo Lee <rbstj0311@naver.com>
2025-01-15 00:05:45 +0900

    fix: delete fetching sarasa mono k and change to load google fonts

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -3,44 +3,6 @@ import { type CollectionEntry } from "astro:content";
 import postOgImage from "./og-templates/post";
 import siteOgImage from "./og-templates/site";
 
-const fetchFonts = async () => {
-  // Regular Font
-  const fontFileRegular = await fetch(
-    "https://github.com/gyunseo/ttf-sarasa-mono-k/raw/main/sarasa-mono-k-regular.ttf"
-  );
-  const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
-
-  // Bold Font
-  const fontFileBold = await fetch(
-    "https://github.com/gyunseo/ttf-sarasa-mono-k/raw/main/sarasa-mono-k-bold.ttf"
-  );
-  const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
-
-  return { fontRegular, fontBold };
-};
-
-const { fontRegular, fontBold } = await fetchFonts();
-
-const options: SatoriOptions = {
-  width: 1200,
-  height: 630,
-  embedFont: true,
-  fonts: [
-    {
-      name: "Sarasa Mono K",
-      data: fontRegular,
-      weight: 400,
-      style: "normal",
-    },
-    {
-      name: "Sarasa Mono K",
-      data: fontBold,
-      weight: 600,
-      style: "normal",
-    },
-  ],
-};
-
 function svgBufferToPngBuffer(svg: string) {
   const resvg = new Resvg(svg);
   const pngData = resvg.render();
